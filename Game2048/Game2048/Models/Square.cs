254c1003664b61054b72e073466c1daaabf67467
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-03-30 18:55:27 +0100

    Create a Square Model class and add a collection of it in the MainViewModel

---
+++ Game2048/Game2048/Models/Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game2048.Models
+{
+    public class Square
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Value { get; set; }
+    }
+}
--- Game2048/Game2048/ViewModels/MainViewModel.cs
+++ Game2048/Game2048/ViewModels/MainViewModel.cs
@@ -1,16 +1,23 @@
-﻿using Game2048.ViewModels.Base;
+﻿using DynamicData;
+using Game2048.Models;
+using Game2048.ViewModels.Base;
 using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using ReactiveUI.Legacy;
 
 namespace Game2048.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public ReactiveList<Square> Squares;
+
         public MainViewModel(IScreen hostScreen = null) : base(hostScreen)
         {
-
+            Squares = new ReactiveList<Square>();
+            Squares.Add(new Square() { X = 0, Y = 0, Value = 2 });
+            Squares.Add(new Square() { X = 1, Y = 0, Value = 2 });
+            Squares.Add(new Square() { X = 2, Y = 1, Value = 2 });
+            Squares.Add(new Square() { X = 0, Y = 2, Value = 2 });
+            Squares.Add(new Square() { X = 3, Y = 2, Value = 4 });
         }
     }
 }
