5814c3a052aa9bf5050499222b90832a14b2a948
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-03-30 18:57:24 +0100

    Replace the static grid with ItemsControl that holds the grid and add the binding between MainViewModel's Square collection and MainView's Grid

--- Game2048/Game2048/Views/MainView.xaml.cs
+++ Game2048/Game2048/Views/MainView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Game2048.ViewModels;
+﻿using Game2048.Models;
+using Game2048.ViewModels;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,5 +19,11 @@ namespace Game2048.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.OneWayBind(ViewModel, vm => vm.Squares, v => v.Board.ItemsSource);
+        }
+    }
 }
