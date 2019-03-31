cd84db44ef3010cfc2e8075276d662ef956e8d6f
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-03-31 22:29:25 +0200

    Create a ContentViewBase class to display custom controls in ItemsControl

---
+++ Game2048/Game2048/Views/ContentViewBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Text;
+using ReactiveUI.XamForms;
+namespace Game2048.Views
+{
+    public class ContentViewBase<TViewModel> : ReactiveContentView<TViewModel> where TViewModel : class
+    {
+        protected readonly CompositeDisposable SubscriptionDisposables = new CompositeDisposable();
+    }
+}
