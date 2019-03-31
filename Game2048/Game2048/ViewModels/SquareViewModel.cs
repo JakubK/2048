d0a807b02a715190c8e7ce454e63e6934a00def9
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-03-31 22:30:03 +0200

    Move Square class contents to SquareViewModel and delete the Square

---
+++ Game2048/Game2048/ViewModels/SquareViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Game2048.ViewModels.Base;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game2048.ViewModels
+{
+    public class SquareViewModel : ViewModelBase
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Value { get; set; }
+
+        public SquareViewModel()
+        {
+        }
+
+        public SquareViewModel(int x, int y,int value)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Value = value;
+        }
+    }
+}
