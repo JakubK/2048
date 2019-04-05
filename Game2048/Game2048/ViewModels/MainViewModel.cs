e80300b35efee01f0909ebc6b358d9e5bcf1a1d9
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-04-05 17:03:24 +0200

    Implement a logic for moving the squares inside the board

--- Game2048/Game2048/ViewModels/MainViewModel.cs
+++ Game2048/Game2048/ViewModels/MainViewModel.cs
@@ -3,14 +3,21 @@ using Game2048.ViewModels.Base;
 using ReactiveUI;
 using ReactiveUI.Legacy;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive;
 
 namespace Game2048.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public ObservableCollection<SquareViewModel> Squares;
+        private ObservableCollection<SquareViewModel> squares;
+        public ObservableCollection<SquareViewModel> Squares
+        {
+            get => squares;
+            set => this.RaiseAndSetIfChanged(ref squares, value);
+        }
 
         public MainViewModel(IScreen hostScreen = null) : base(hostScreen)
         {
@@ -22,15 +29,263 @@ namespace Game2048.ViewModels
             Squares.Add(new SquareViewModel() { X = 0, Y = 2, Value = 2 });
             Squares.Add(new SquareViewModel() { X = 3, Y = 2, Value = 4 });
 
-            MoveLeft = ReactiveCommand.Create(() => System.Diagnostics.Debug.WriteLine("LEFT"));
-            MoveRight = ReactiveCommand.Create(() => System.Diagnostics.Debug.WriteLine("RIGHT"));
-            MoveUp = ReactiveCommand.Create(() => System.Diagnostics.Debug.WriteLine("UP"));
-            MoveDown = ReactiveCommand.Create(() => System.Diagnostics.Debug.WriteLine("DOWN"));
+            MoveLeft = ReactiveCommand.Create(() =>  MoveToLeft());
+            MoveRight = ReactiveCommand.Create(() => MoveToRight());
+            MoveUp = ReactiveCommand.Create(() => MoveToTop());
+            MoveDown = ReactiveCommand.Create(() => MoveToBottom());
         }
 
         public ReactiveCommand<Unit, Unit> MoveLeft { get; }
         public ReactiveCommand<Unit, Unit> MoveRight { get; }
         public ReactiveCommand<Unit, Unit> MoveUp { get; }
         public ReactiveCommand<Unit, Unit> MoveDown { get; }
+
+        private void MoveToLeft()
+        {
+            var rows = Squares.GroupBy(r => r.Y).ToList();
+            foreach(var row in rows)
+            {
+                MoveRowToLeft(row.OrderBy(x => x.X).ToList());
+            }
+        }
+
+        private void MoveRowToLeft(List<SquareViewModel> row)
+        {
+            bool substituteCollection = false;
+            foreach(SquareViewModel item in row)
+            {
+                bool finishedMovingItem = false;
+                while(!finishedMovingItem)
+                {
+                    if(item.X == 0)
+                    {
+                        finishedMovingItem = true;
+                    }
+                    else
+                    {
+                        SquareViewModel neighbourElement = null;
+                        for(int i = 0;i < Squares.Count;i++)
+                        {
+                            if (Squares[i].Y == item.Y && Squares[i].X == item.X - 1)
+                            {
+                                neighbourElement = Squares[i];
+                                break;
+                            }
+                        }
+
+                        if(neighbourElement != null)
+                        {
+                            if(neighbourElement.Value == item.Value)
+                            {
+                                neighbourElement.Value *= 2;
+                                Squares.Remove(item);
+                                substituteCollection = true;
+
+                                finishedMovingItem = true;
+                            }
+                            else
+                            {
+                                finishedMovingItem = true;
+                            }
+                        }
+                        else
+                        {
+                            item.X--;
+                        }
+                    }
+                }
+            }
+
+            if (substituteCollection)
+                SubstituteCollection();
+        }
+
+        private void MoveToRight()
+        {
+            var rows = Squares.GroupBy(r => r.Y).ToList();
+            foreach (var row in rows)
+            {
+                MoveRowToRight(row.OrderByDescending(x => x.X).ToList());
+            }
+        }
+
+        private void MoveRowToRight(List<SquareViewModel> row)
+        {
+            bool substituteCollection = false;
+            foreach (SquareViewModel item in row)
+            {
+                bool finishedMovingItem = false;
+                while (!finishedMovingItem)
+                {
+                    if (item.X == 3)
+                    {
+                        finishedMovingItem = true;
+                    }
+                    else
+                    {
+                        SquareViewModel neighbourElement = null;
+                        for (int i = 0; i < Squares.Count; i++)
+                        {
+                            if (Squares[i].Y == item.Y && Squares[i].X == item.X + 1)
+                            {
+                                neighbourElement = Squares[i];
+                                break;
+                            }
+                        }
+
+                        if (neighbourElement != null)
+                        {
+                            if (neighbourElement.Value == item.Value)
+                            {
+                                neighbourElement.Value *= 2;
+                                Squares.Remove(item);
+                                substituteCollection = true;
+
+                                finishedMovingItem = true;
+                            }
+                            else
+                            {
+                                finishedMovingItem = true;
+                            }
+                        }
+                        else
+                        {
+                            item.X++;
+                        }
+                    }
+                }
+            }
+            if (substituteCollection)
+                SubstituteCollection();
+        }
+
+        private void MoveToBottom()
+        {
+            var cols = Squares.GroupBy(r => r.X).ToList();
+            foreach (var col in cols)
+            {
+                MoveColToBottom(col.OrderByDescending(x => x.Y).ToList());                
+            }
+        }
+
+        private void MoveColToBottom(List<SquareViewModel> col)
+        {
+            bool substituteCollection = false;
+            foreach (SquareViewModel item in col)
+            {
+                bool finishedMovingItem = false;
+                while (!finishedMovingItem)
+                {
+                    if (item.Y == 3)
+                    {
+                        finishedMovingItem = true;
+                    }
+                    else
+                    {
+                        SquareViewModel neighbourElement = null;
+                        for (int i = 0; i < Squares.Count; i++)
+                        {
+                            if (Squares[i].X == item.X && Squares[i].Y == item.Y + 1)
+                            {
+                                neighbourElement = Squares[i];
+                                break;
+                            }
+                        }
+
+                        if (neighbourElement != null)
+                        {
+                            if (neighbourElement.Value == item.Value)
+                            {
+                                neighbourElement.Value *= 2;
+                                Squares.Remove(item);
+                                substituteCollection = true;
+
+                                finishedMovingItem = true;
+                            }
+                            else
+                            {
+                                finishedMovingItem = true;
+                            }
+                        }
+                        else
+                        {
+                            item.Y++;
+                        }
+                    }
+                }
+            }
+            if (substituteCollection)
+                SubstituteCollection();
+        }
+
+
+        private void MoveToTop()
+        {
+            var cols = Squares.GroupBy(r => r.X).ToList();
+            foreach (var col in cols)
+            {
+                MoveColToTop(col.OrderBy(x => x.Y).ToList());
+            }
+        }
+
+        private void MoveColToTop(List<SquareViewModel> col)
+        {
+            bool substituteCollection = false;
+            foreach (SquareViewModel item in col)
+            {
+                bool finishedMovingItem = false;
+                while (!finishedMovingItem)
+                {
+                    if (item.Y == 0)
+                    {
+                        finishedMovingItem = true;
+                    }
+                    else
+                    {
+                        SquareViewModel neighbourElement = null;
+                        for (int i = 0; i < Squares.Count; i++)
+                        {
+                            if (Squares[i].X == item.X && Squares[i].Y == item.Y - 1)
+                            {
+                                neighbourElement = Squares[i];
+                                break;
+                            }
+                        }
+
+                        if (neighbourElement != null)
+                        {
+                            if (neighbourElement.Value == item.Value)
+                            {
+                                neighbourElement.Value *= 2;
+                                Squares.Remove(item);
+                                substituteCollection = true;
+
+                                finishedMovingItem = true;
+                            }
+                            else
+                            {
+                                finishedMovingItem = true;
+                            }
+                        }
+                        else
+                        {
+                            item.Y--;
+                        }
+                    }
+                }
+            }
+            if (substituteCollection)
+                SubstituteCollection();
+        }
+
+        private void SubstituteCollection()
+        {
+            ObservableCollection<SquareViewModel> squareCopy = new ObservableCollection<SquareViewModel>();
+            for (int i = 0; i < Squares.Count; i++)
+            {
+                squareCopy.Add(Squares[i]);
+            }
+            Squares = squareCopy;
+        }
     }
 }
