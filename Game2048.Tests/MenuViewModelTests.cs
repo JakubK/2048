f289606bbff2942ae43635dfceabaec78320a34c
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-04-24 17:31:35 +0200

    Create MenuViewModelTests class with 2 simple test for checking if the index of collection of boards won't go out of it's limit and remove generated UnitTest1 file

---
+++ Game2048.Tests/MenuViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using Game2048.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game2048.Tests
+{
+    public class MenuViewModelTests
+    {
+        [Test]
+        public void IncrementDimension_WhenReachedLimit_DoesNothing()
+        {
+            //arrange
+            MenuViewModel sut = new MenuViewModel();
+            sut.SelectedDimension = 8;
+            int startVal = sut.SelectedDimension;
+            //act
+            sut.IncrementDimension.Execute();
+            //assert
+            Assert.That(sut.SelectedDimension == 8);
+        }
+
+        [Test]
+        public void DecrementDimension_WhenReachedLimit_DoesNothing()
+        {
+            //arrange
+            MenuViewModel sut = new MenuViewModel();
+            sut.SelectedDimension = 3;
+            int startVal = sut.SelectedDimension;
+            //act
+            sut.DecrementDimension.Execute();
+            //assert
+            Assert.That(sut.SelectedDimension == 3);
+        }
+    }
+}
