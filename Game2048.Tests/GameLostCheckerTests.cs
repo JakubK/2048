1631246770801b2c744da3e35437c6ea47828f03
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-05-01 13:59:19 +0200

    Fix arrange section in the IsLost_WhenBoardFilledButOptionToMergeVertically_ReturnsFalse Unit Test

--- Game2048.Tests/GameLostCheckerTests.cs
+++ Game2048.Tests/GameLostCheckerTests.cs
@@ -76,12 +76,12 @@ namespace Game2048.Tests
                 new SquareViewModel{X = 0, Y = 0, Value = 1},
                 new SquareViewModel{X = 1, Y = 0, Value = 2},
                 new SquareViewModel{X = 2, Y = 0 , Value = 3},
-                new SquareViewModel{X = 0, Y = 1 , Value = 4},
+                new SquareViewModel{X = 0, Y = 1 , Value = 1},
                 new SquareViewModel{X = 1, Y = 1 , Value = 5},
                 new SquareViewModel{X = 2, Y = 1 , Value = 6},
                 new SquareViewModel{X = 0, Y = 2 , Value = 7},
                 new SquareViewModel{X = 1, Y = 2 , Value = 8},
-                new SquareViewModel{X = 2, Y = 2 , Value = 8},
+                new SquareViewModel{X = 2, Y = 2 , Value = 9},
             });
 
             //act
