6c9fb50b2be9e075d71b8ddf931b77ac506c7c26
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-05-16 20:42:39 +0200

    Add 4 unit test methods for checking Drag Direction detection in the DragReaderTests

--- Game2048.Tests/DragReaderTests.cs
+++ Game2048.Tests/DragReaderTests.cs
@@ -36,5 +36,57 @@ namespace Game2048.Tests
             //assert
             Assert.That(response == MoveDirection.None);
         }
+
+        [Test]
+        public void GetDirection_WhenPannedRight_ReturnsRightMoveDirectionType()
+        {
+            //arrange
+            var sut = new DragReader();
+            MoveDirection response;
+            //act
+            sut.GetDirection(new PanUpdatedEventArgs(GestureStatus.Started, 0, 500, 0));
+            response = sut.GetDirection(new PanUpdatedEventArgs(GestureStatus.Completed, 0, 0, 0));
+            //assert
+            Assert.That(response == MoveDirection.Right);
+        }
+
+        [Test]
+        public void GetDirection_WhenPannedLeft_ReturnsLeftMoveDirectionType()
+        {
+            //arrange
+            var sut = new DragReader();
+            MoveDirection response;
+            //act
+            sut.GetDirection(new PanUpdatedEventArgs(GestureStatus.Started, 0, -500, 0));
+            response = sut.GetDirection(new PanUpdatedEventArgs(GestureStatus.Completed, 0, 0, 0));
+            //assert
+            Assert.That(response == MoveDirection.Left);
+        }
+
+        [Test]
+        public void GetDirection_WhenPannedTop_ReturnsTopMoveDirectionType()
+        {
+            //arrange
+            var sut = new DragReader();
+            MoveDirection response;
+            //act
+            sut.GetDirection(new PanUpdatedEventArgs(GestureStatus.Started, 0, 0, -500));
+            response = sut.GetDirection(new PanUpdatedEventArgs(GestureStatus.Completed, 0, 0, 0));
+            //assert
+            Assert.That(response == MoveDirection.Top);
+        }
+
+        [Test]
+        public void GetDirection_WhenPannedBottom_ReturnsBottomMoveDirectionType()
+        {
+            //arrange
+            var sut = new DragReader();
+            MoveDirection response;
+            //act
+            sut.GetDirection(new PanUpdatedEventArgs(GestureStatus.Started, 0, 0, 500));
+            response = sut.GetDirection(new PanUpdatedEventArgs(GestureStatus.Completed, 0, 0, 0));
+            //assert
+            Assert.That(response == MoveDirection.Bottom);
+        }
     }
 }
